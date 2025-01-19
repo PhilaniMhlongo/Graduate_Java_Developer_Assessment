63bd972641bc5d464987c606f43f2139585f57d0
mhlongophilani <mhlongophilani04@gmail.com>
2025-01-19 17:03:32 +0200

    Files from String.io

---
+++ src/main/java/com/enviro/assessment/grad001/philanimhlongo/PhilanimhlongoApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.philanimhlongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhilanimhlongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhilanimhlongoApplication.class, args);
+	}
+
+}
